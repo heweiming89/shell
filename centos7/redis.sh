226c315e12423b7aed07cb4a2a75e8350a690f7b
heweiming <heweiming89@163.com>
2021-07-24 02:13:24 +0800

    增加说明

--- centos7/redis.sh
+++ centos7/redis.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "拉取 redis 镜像"
 docker pull redis:6
 
+echo "创建 redis 相关目录"
 # shellcheck disable=SC2174
 mkdir -m 777 -p /home/docker/redis/conf
 # shellcheck disable=SC2174
 mkdir -m 777 -p /home/docker/redis/data
 
+echo "运行redis"
 tee /home/docker/redis/conf/redis.conf <<-'EOF'
 port 6379
 bind 0.0.0.0
