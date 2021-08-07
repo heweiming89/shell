b740c64c1729c68b10299741aec7697279d2a92d
heweiming <heweiming89@163.com>
2021-08-07 22:08:03 +0800

    rabbitmq_management

--- centos7/rabbitmq.sh
+++ centos7/rabbitmq.sh
@@ -23,6 +23,7 @@ cd /usr/local/src/rabbitmq
 wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/3.8.17/rabbitmq_delayed_message_exchange-3.8.17.8f537ac.ez
 docker cp rabbitmq_delayed_message_exchange-3.8.17.8f537ac.ez rabbitmq:/plugins
 
+docker exec -it rabbitmq rabbitmq-plugins enable rabbitmq_management
 docker exec -it rabbitmq rabbitmq-plugins enable rabbitmq_delayed_message_exchange
 docker exec -it rabbitmq rabbitmq-plugins list
 
