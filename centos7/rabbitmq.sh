da20f58c94a6619d32532cb87b1f1ed94f76eee4
heweiming <heweiming89@163.com>
2021-08-07 22:26:18 +0800

    rabbitmq 时间问题

--- centos7/rabbitmq.sh
+++ centos7/rabbitmq.sh
@@ -23,6 +23,10 @@ cd /usr/local/src/rabbitmq
 wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/3.8.17/rabbitmq_delayed_message_exchange-3.8.17.8f537ac.ez
 docker cp rabbitmq_delayed_message_exchange-3.8.17.8f537ac.ez rabbitmq:/plugins
 
+docker exec -it rabbitmq mkdir -p /usr/share/zoneinfo/Asia
+docker cp /usr/share/zoneinfo/Asia/Shanghai rabbitmq:/usr/share/zoneinfo/Asia/Shanghai
+docker exec -it rabbitmq ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
 docker exec -it rabbitmq rabbitmq-plugins enable rabbitmq_management
 docker exec -it rabbitmq rabbitmq-plugins enable rabbitmq_delayed_message_exchange
 docker exec -it rabbitmq rabbitmq-plugins list
